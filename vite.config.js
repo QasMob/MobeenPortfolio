b13f7e73a905411c330630f6059f93eadb22257f
Mobeen Qasim <mobeenqq@gmail.com>
2024-08-29 23:21:04 -0400

    resume

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         pipeline: resolve(__dirname, 'pipeline.html'),
+        Mobeen_Qasim_Resume_PDF: resolve(__dirname, './public/Mobeen_Qasim_Resume_PDF.pdf'),
       },
     },
   },

197ba2281eba3f3e2a9559206782bc121c89fe64
Mobeen Qasim <mobeenqq@gmail.com>
2024-08-30 22:26:59 -0400

    growthub

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         pipeline: resolve(__dirname, 'pipeline.html'),
+        growth: resolve(__dirname, 'growth.html')
       },
     },
   },
